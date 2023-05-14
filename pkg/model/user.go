21bca85037290b82441ccd5b44a1f27e5be84c8f
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-14 14:19:08 -0700

    user structure

---
+++ pkg/model/user.go
@@ -0,0 +1,9 @@
+package model
+
+type User struct {
+	Id       int    `json:"id"`
+	Uuid     string `json:"uuid"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
