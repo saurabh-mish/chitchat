d3dd90d27c3046c935cbab9ccc7f968212c9c686
Saurabh Mishra <saurabh.mish@icloud.com>
2023-05-16 23:28:37 -0700

    check existing user by ID and non-existing user

--- pkg/handler/user_test.go
+++ pkg/handler/user_test.go
@@ -21,6 +21,39 @@ func TestGetAllUsers(t *testing.T) {
 
 	err := json.NewDecoder(got).Decode(&want)
 	if err != nil {
-		t.Errorf("Response %v doesn't match User structure %v", got, err)
+		t.Fatalf("Response %v doesn't match User structure %v", got, err)
 	}
+}
+
+func TestGetUserById(t *testing.T) {
+
+	t.Run("get existing user", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/api/v1/users/567", nil)
+		response := httptest.NewRecorder()
+
+		GetUserById(response, request)
+
+		got := response.Body
+		var want model.User
+
+		err := json.NewDecoder(got).Decode(&want)
+		if err != nil {
+			t.Fatalf("Response %v doesn't match user structure %v", got, err)
+		}
+	})
+
+	t.Run("get non-existing user", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/api/v1/users/941", nil)
+		response := httptest.NewRecorder()
+
+		GetUserById(response, request)
+
+		got := response.Code
+		var want int = 404
+
+		if got != want {
+			t.Fatal("User should not exist")
+		}
+	})
+
 }
